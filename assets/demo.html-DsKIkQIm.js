82fcc4f8b6aa5833e4d97defc1035e750ea5830b
Azil <13701378834@163.com>
2024-05-11 16:35:31 +0800

    'deploy'

---
+++ assets/demo.html-DsKIkQIm.js
@@ -0,0 +1,11 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{o as t,c as o,a as n}from"./app-Dya65-u5.js";const r={};function l(a,m){return t(),o("div",null,[n(` <details>
+<summary>点击以展开代码</summary>
+
+\`\`\`python
+def hello_world():
+    print("Hello, World!")
+
+hello_world()
+\`\`\`
+
+</details> `)])}const c=e(r,[["render",l],["__file","demo.html.vue"]]),p=JSON.parse('{"path":"/ydfel/%E6%96%87%E4%BB%B6%E9%A2%84%E8%A7%88%E7%BB%84%E4%BB%B6/demo.html","title":"使用案例","lang":"zh-CN","frontmatter":{"title":"使用案例","head":[["meta",{"property":"og:url","content":"http://localhost:8080/ydfel/%E6%96%87%E4%BB%B6%E9%A2%84%E8%A7%88%E7%BB%84%E4%BB%B6/demo.html"}],["meta",{"property":"og:site_name","content":"Azil"}],["meta",{"property":"og:title","content":"使用案例"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-05-07T09:38:47.000Z"}],["meta",{"property":"article:author","content":"Azil"}],["meta",{"property":"article:modified_time","content":"2024-05-07T09:38:47.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"使用案例\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-05-07T09:38:47.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Azil\\",\\"url\\":\\"https://gitee.com/codedi\\"}]}"]]},"headers":[],"git":{"createdTime":1713864292000,"updatedTime":1715074727000,"contributors":[{"name":"Azil","email":"13701378834@163.com","commits":1}]},"readingTime":{"minutes":0.08,"words":23},"filePathRelative":"ydfel/文件预览组件/demo.md","localizedDate":"2024年4月23日","excerpt":"<!-- <details>\\n<summary>点击以展开代码</summary>\\n\\n```python\\ndef hello_world():\\n    print(\\"Hello, World!\\")\\n\\nhello_world()\\n```\\n\\n</details> -->\\n"}');export{c as comp,p as data};
